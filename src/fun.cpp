2a8f3f5433ff72f52a1f29e6fa39a8527c79fd9a
Drew2041 <101018333+Drew2041@users.noreply.github.com>
2022-03-05 23:34:23 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,9 +1,8 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
-
 int64_t power(int64_t x, uint16_t n) {
     int64_t result = 1;
-    for ( int i =1;i<n;i++)
+    for ( int i =1; i < n; i++)
         result *= x;
     return result;
 }
