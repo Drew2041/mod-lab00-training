43d4bb2ea17f68dcdb94abcf22150649799d73d8
Drew2041 <101018333+Drew2041@users.noreply.github.com>
2022-03-05 23:53:39 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
-#include <iostream>
-using std::cout;
- using std::cin;
- setlocale(LC_ALL,"Russian")
+
 int main() {
-   
-    int a,b;
-cin >> a;
-cin >> b;
-cout << power(a,b);
+ int x = power(2, 5);
+ return x;
 }
