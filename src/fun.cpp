f539bffaef2d70fdffad61424811e7a512db57fc
Drew2041 <101018333+Drew2041@users.noreply.github.com>
2022-03-05 23:36:55 +0300

    Устранение ошибок

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,7 +2,7 @@
 #include "fun.h"
 int64_t power(int64_t x, uint16_t n) {
     int64_t result = 1;
-    for ( int i =1; i < n; i++)
+    for ( int i = 1; i < n; i++)
         result *= x;
     return result;
 }
